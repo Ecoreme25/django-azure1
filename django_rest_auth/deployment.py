172e259043f57b895363eacd0c7600aab877877d
Ola1 <ecoremecustomercare@gmail.com>
2025-08-24 07:48:59 +0100

    Let env variables be picked auto

--- django_rest_auth/deployment.py
+++ django_rest_auth/deployment.py
@@ -2,8 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-ALLOWED_HOSTS = ["django-backend1.azurewebsites.net", "localhost"]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
